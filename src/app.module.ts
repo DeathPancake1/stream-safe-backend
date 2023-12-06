91d9997af39b7b936616768ef03139503f44d0c8
Youssef Dawoud <youssefyoyododo@gmail.com>
2023-12-06 02:10:26 +0200

    Added User module with ability to create a user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UploadFileModule } from './upload-file/upload-file.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UploadFileModule, AuthModule, ConfigModule.forRoot() ],
+  imports: [UploadFileModule, AuthModule, ConfigModule.forRoot(), UserModule ],
   controllers: [AppController],
   providers: [AppService],
 })
