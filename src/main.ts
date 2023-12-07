e1bddf7ab4dcd40d166936a0dbd0d265ffae8c77
Youssef Dawoud <youssefyoyododo@gmail.com>
2023-12-07 16:59:19 +0200

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
