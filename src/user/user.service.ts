d9282368c44babf9ed3a950cc4a19aa4b2091979
Youssef Dawoud <youssefyoyododo@gmail.com>
2024-01-26 20:48:45 +0200

    Fixed error in search

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -67,6 +67,11 @@ export class UserService {
                         email:{
                             not:userEmailFromToken
                         }
+                    },
+                    {
+                        publicKey: {
+                            not: null
+                        }
                     }
                 ]
                 
